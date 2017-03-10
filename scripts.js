c56e5b12c611a5ea79517049c05ca6620ccc9cf2
LucasJoaquim <lucasjoaquimjr@gmail.com>
2017-03-09 21:39:18 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,9 +3,11 @@ mudar = function(){
   var y = document.getElementById('menu');
   if( x.style.display === 'none') {
     x.style.display = 'block';
-    y.src = 'menu-preto.svg';                               
+    y.src = 'menu-preto.svg';
+    y.background = 'white';
     return;
   }
   x.style.display = 'none';
-  y.src = 'menu.svg';   
+  y.src = 'menu.svg';
+  y.background = '';
 }
