29fa5e21df32c05b4217b45fef0e94c2d23292a1
LucasJoaquim <lucasjoaquimjr@gmail.com>
2017-03-17 22:49:23 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
-mudar = function(){
+function mudar(){
   var x = document.getElementById('overlay');
   if( x.style.display === 'none') {
     x.style.display = 'block';
     return;
   }
   x.style.display = 'none';
+}
+
+function data() {
+	var d = new Date();
+	var texto =document.getElementById("a");
+	texto.innerHTML = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear();
 }
